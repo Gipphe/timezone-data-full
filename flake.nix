27ced0053b6b4e0b8ac8829a4fa20cd512f8a63a
Victor Nascimento Bakke <gipphe@gmail.com>
2025-09-05 20:46:51 +0200

    chore: add scripts for auto publishing

--- flake.nix
+++ flake.nix
@@ -9,11 +9,83 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+        python = pkgs.python312.withPackages (p: with p; [ feedparser ]);
       in
       {
+        apps = {
+          build = {
+            type = "app";
+            program = (
+              lib.getExe (
+                pkgs.writeShellApplication {
+                  name = "build";
+                  runtimeInputs = with pkgs; [
+                    python
+                    elmPackages.elm-format
+                    git
+                    bash
+                  ];
+                  text = ''
+                    bash ./build.sh "" "$1"
+                  '';
+                }
+              )
+            );
+          };
+          build-on-new-version = {
+            type = "app";
+            program = (
+              lib.getExe (
+                pkgs.writeShellApplication {
+                  name = "check-feed";
+                  runtimeInputs = with pkgs; [
+                    jq
+                    jo
+                  ];
+                  text = ''
+                    if test "$#" -lt 1 || test "$#" -gt 2; then
+                      echo "Unexpected number of arguments: $#" >&2
+                      exit 1
+                    fi
+
+                    if ! test -f "$1"; then
+                      echo "Passed argument is not the file path of an existing file" >&2
+                      exit 1
+                    fi
+
+                    newest_entry="$(jq -c '.entries[0]' "$1")"
+                    new_timestamp="$(echo "$newest_entry" | jq '.published')"
+                    version="$(echo "$newest_entry" | jq '.title')"
+                    timestamp_file=''${2:-last_timestamp}
+                    timestamp='0'
+                    if test -f "$timestamp_file"; then
+                      timestamp="$(cat "$timestamp_file")"
+                    fi
+                    is_new="false"
+                    if test "$timestamp" != "$new_timestamp"; then
+                      is_new="true"
+                      timestamp="$new_timestamp"
+                      echo "$new_timestamp" > "$timestamp_file"
+                    fi
+
+                    if test "$is_new" != "true"; then
+                      echo "Not newer" >&2
+                      exit 0
+                    fi
+
+                    nix run .#build -- "$version" 1>&2
+
+                    jo is_new="$is_new" version="$version" timestamp="$timestamp"
+                  '';
+                }
+              )
+            );
+          };
+        };
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.python312
+            python
             pkgs.elmPackages.elm-format
             pkgs.git
           ];
